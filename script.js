959fdeaa31510b3ab1be1772403e4677032cd0be
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-06 23:05:01 -0700

    added js to the page functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// variables
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
+// adding eventlistners
+open.addEventListener('click', () => container.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
